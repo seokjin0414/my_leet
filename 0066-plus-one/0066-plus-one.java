0a59f32e819f1b7f9e8fbad80caa234375914d90
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-11-29 00:25:59 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (18.22%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        int index = digits.length - 1;
+
+        while(index >= 0 && carry > 0){
+            digits[index] = (digits[index] + 1) % 10;
+            carry = (digits[index] == 0)? 1 : 0;
+
+            index--;
+        }
+
+        if(carry > 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+
+        return digits;
+    }
+}
