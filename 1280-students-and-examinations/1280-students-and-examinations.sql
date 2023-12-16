a783b5258dff9f454d7f55a15826b8a66e16177e
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-12-16 23:08:24 +0900

    Time: 1552 ms (73.00%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,3 +1,4 @@
+
 SELECT s.student_id, s.student_name, sub.subject_name, COALESCE(e.attended_exams, 0) AS attended_exams
 FROM Students s
 CROSS JOIN Subjects sub
@@ -5,5 +6,5 @@ LEFT JOIN (
     SELECT student_id, subject_name, COUNT(*) AS attended_exams
     FROM Examinations
     GROUP BY student_id, subject_name
-) e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+) e USING (student_id, subject_name)
 ORDER BY s.student_id, sub.subject_name;
