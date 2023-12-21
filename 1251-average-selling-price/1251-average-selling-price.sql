4f83f1fd5585d3e6d53b46d4162f0c399316358a
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-12-22 00:05:34 +0900

    Time: 1553 ms (25.24%), Space: 0B (100.00%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, IFNULL(ROUND(SUM(u.units * p.price) / SUM(u.units), 2), 0) average_price
+FROM Prices p
+LEFT JOIN UnitsSold u 
+ON u.product_id = p.product_id 
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY 1
