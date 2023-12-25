1511ed982f8e29df75c4be26ede0bd10d7d0828b
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-12-25 21:21:42 +0900

    Time: 1537 ms (81.25%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-SELECT contest_id, ROUND(COUNT(DISTINCT user_id) * 100 /(SELECT COUNT(user_id) FROM Users) ,2) percentage
+SELECT contest_id, ROUND(COUNT(DISTINCT user_id) * 100 / (SELECT COUNT(user_id) FROM Users) ,2) percentage
 FROM  Register
 GROUP BY 1
 ORDER BY 2 DESC,1 ASC
