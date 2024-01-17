1d613230949fe3ae4c0a83937e55c800b5c00c1d
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-01-17 23:57:46 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (36.76%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,9 +3,7 @@ class Solution {
         int n = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                int temp = nums[n];
                 nums[n] = nums[i];
-                nums[i] = temp;
                 n++;
             }
         }
