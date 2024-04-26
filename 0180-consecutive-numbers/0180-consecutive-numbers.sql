497025d5135ce606e2bf9ac2cbb50425d4277980
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-04-26 23:23:45 +0900

    Time: 843 ms (92.12%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,7 +1,4 @@
-WITH all_logs AS (
-    SELECT * FROM Logs
-)
 SELECT DISTINCT Num ConsecutiveNums
 FROM Logs
-WHERE (Id + 1, Num) IN (SELECT * FROM all_logs)
-    AND (Id + 2, Num) IN (SELECT * FROM all_logs)
+WHERE (Id + 1, Num) IN (SELECT * FROM Logs)
+    AND (Id + 2, Num) IN (SELECT * FROM Logs)
