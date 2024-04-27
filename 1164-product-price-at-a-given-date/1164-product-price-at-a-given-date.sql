955c3d1a325855ba5e42ad103cf6710a04fa05f2
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-04-27 19:19:34 +0900

    Time: 797 ms (81.76%), Space: 0B (100.00%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,13 @@
+WITH vi AS(
+    SELECT *, RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) n 
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+
+SELECT product_id, new_price price
+FROM vi 
+WHERE n = 1
+UNION
+SELECT product_id, 10 price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id FROM vi)
