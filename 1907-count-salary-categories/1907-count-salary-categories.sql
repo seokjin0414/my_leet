69fcfee738cf8914f2adb103a4ac8528504eabb4
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-05-05 20:37:16 +0900

    Time: 2664 ms (52.50%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,8 +1,11 @@
+WITH vi AS(
+SELECT * FROM Accounts
+)
 SELECT "Low Salary" category, SUM(income < 20000) accounts_count
-FROM Accounts
+FROM vi
 UNION
 SELECT "Average Salary" category, SUM(income BETWEEN 20000 AND 50000) accounts_count
-FROM Accounts
+FROM vi
 UNION
 SELECT "High Salary" category, SUM(income > 50000) accounts_count
-FROM Accounts;
+FROM vi;
