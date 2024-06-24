7864d668372f299be9b3c842a530f8f887c8aff0
seokjin shin <sars21@hanmail.net>
2024-06-24 20:40:32 +0900

    Time: 337 ms (85.12%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -8,7 +8,6 @@ day7_sum AS (
     SELECT visited_on, SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) amount
     FROM day_sum
 )
-
 SELECT visited_on, amount, ROUND(amount / 7, 2) average_amount
 FROM day7_sum
 WHERE visited_on > (SELECT MIN(visited_on) FROM day_sum) + INTERVAL 5 DAY
