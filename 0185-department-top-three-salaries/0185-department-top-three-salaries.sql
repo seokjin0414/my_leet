60dd60b8cbcb86fe800c725e56f74b533792b0fb
seokjin shin <sars21@hanmail.net>
2024-06-30 23:19:15 +0900

    Time: 745 ms (94.41%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,13 @@
+WITH base AS (
+    SELECT 
+        d.name Department, 
+        e.name Employee, 
+        e.salary Salary, 
+        DENSE_RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC) d_num
+    FROM Employee e
+    INNER JOIN Department d ON e.departmentId = d.id  
+)
+SELECT Department, Employee, Salary
+FROM base
+WHERE d_num <= 3
+ORDER BY 1, 3 DESC;
