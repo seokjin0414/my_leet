8025bfa23917b05d044a09aa5b71160ddbf33eaf
seokjin shin <sars21@hanmail.net>
2024-07-26 10:00:11 +0900

    Time: 1 ms (74.27%), Space: 2.2 MB (31.85%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut k = (m + n - 1) as usize;
+        let mut i = (m - 1) as isize;
+        let mut j = (n - 1) as isize;
+
+        while j >= 0 {
+            if i >= 0 && nums1[i as usize] > nums2[j as usize] {
+                nums1[k] = nums1[i as usize];
+                i -= 1;
+            } else {
+                nums1[k] = nums2[j as usize];
+                j -= 1;
+            }
+            k -= 1;
+        }     
+    }
+}
