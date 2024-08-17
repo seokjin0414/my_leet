4eaba725b882d5f939c61e62c8427a7890c0db88
seokjin shin <sars21@hanmail.net>
2024-08-17 23:18:16 +0900

    Time: 1215 ms (5.04%), Space: 0B (100%) - LeetHub

---
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/1536-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,6 @@
+SELECT c.customer_id, c.customer_name
+FROM Customers c
+INNER JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id
+HAVING SUM(o.product_name = "A") > 0 AND SUM(o.product_name = "B") > 0 AND SUM(o.product_name = "C") = 0
+ORDER BY 1
