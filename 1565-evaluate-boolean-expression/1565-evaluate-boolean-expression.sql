0e2e245abcea8c401c3d666b1b239ea146e2c11d
seokjin shin <sars21@hanmail.net>
2024-08-20 08:40:16 +0900

    Time: 951 ms (86.83%), Space: 0B (100%) - LeetHub

--- 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
@@ -1,8 +1,8 @@
 SELECT e.*,
-    CASE WHEN operator = '>' THEN IF(v1.value > v2.value, 'true', 'false')
-        WHEN operator = '<' THEN IF(v1.value < v2.value, 'true', 'false')
-        ELSE IF(v1.value = v2.value, 'true', 'false') 
-    END value
+    IF(CASE WHEN operator = '>' THEN v1.value > v2.value
+        WHEN operator = '<' THEN v1.value < v2.value
+        ELSE v1.value = v2.value
+    END, "true", "false") value
 FROM Expressions e
 INNER JOIN Variables v1 ON e.left_operand = v1.name
 INNER JOIN Variables v2 ON e.right_operand = v2.name
