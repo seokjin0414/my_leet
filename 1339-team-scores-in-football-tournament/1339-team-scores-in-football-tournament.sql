66acb658862e83bc0ff086929d76c430067c96aa
seokjin shin <sars21@hanmail.net>
2024-08-25 23:23:06 +0900

    Time: 830 ms (32.36%), Space: 0B (100%) - LeetHub

--- 1339-team-scores-in-football-tournament/1339-team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/1339-team-scores-in-football-tournament.sql
@@ -1,8 +1,8 @@
 SELECT t.team_id, t.team_name,
-    SUM(CASE WHEN team_id=host_team AND host_goals>guest_goals THEN 3
-        WHEN team_id=guest_team AND guest_goals>host_goals THEN 3
-        WHEN team_id=host_team AND host_goals=guest_goals THEN 1
-        WHEN team_id=guest_team AND guest_goals=host_goals THEN 1 ELSE 0 END) num_points
+    SUM(CASE WHEN t.team_id = m.host_team AND m.host_goals > m.guest_goals THEN 3
+        WHEN t.team_id = m.guest_team AND m.guest_goals > m.host_goals THEN 3
+        WHEN t.team_id = m.host_team AND m.host_goals = m.guest_goals THEN 1
+        WHEN t.team_id = m.guest_team AND m.guest_goals = m.host_goals THEN 1 ELSE 0 END) num_points
 FROM Teams t
 LEFT JOIN Matches m ON t.team_id = m.host_team OR t.team_id = m.guest_team
 GROUP BY 1
