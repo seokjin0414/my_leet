07182c47360adf131c8ccf5af316cd6846db9569
seokjin shin <sars21@hanmail.net>
2024-08-25 23:16:17 +0900

    Time: 454 ms (98.62%), Space: 0B (100%) - LeetHub

--- 1842-number-of-calls-between-two-persons/1842-number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/1842-number-of-calls-between-two-persons.sql
@@ -1,6 +1,7 @@
-SELECT LEAST(from_id,to_id) person1,
-GREATEST(from_id,to_id) person2,
-COUNT(duration) call_count,
-SUM(duration) total_duration
+SELECT 
+    CASE WHEN from_id > to_id THEN to_id ELSE from_id END person1,
+    CASE WHEN from_id > to_id THEN from_id ELSE to_id END person2,
+    COUNT(duration) call_count,
+    SUM(duration) total_duration       
 FROM Calls
-GROUP BY 1, 2;
+GROUP BY 2, 1
