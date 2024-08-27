c0806457a0b0be391c31c84d1d0c6d31745b2c8c
seokjin shin <sars21@hanmail.net>
2024-08-27 09:00:41 +0900

    Time: 0 ms (100%), Space: 44.6 MB (69.1%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<Integer> postorder(Node root) {
-        if (root == null) return new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        Stack<Node> s = new Stack<>();
-        s.push(root);
-
-        while (!s.isEmpty()) {
-            Node n = s.pop();
-            list.add(0, n.val);
-            for (Node child : n.children) {
-                s.push(child);
-            }
+        if (root == null) {
+            return new ArrayList<>();
         }
+
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
         return list;
+    }
+
+    private void dfs(Node root, List<Integer> l) {
+        for (Node c : root.children) {
+            dfs(c, l);
+        }
+        l.add(root.val);
     }
 }
