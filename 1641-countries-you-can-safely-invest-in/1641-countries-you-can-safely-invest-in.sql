8a0698ec8f5003867cbd4fb93f50fa90c4f40c25
seokjin shin <sars21@hanmail.net>
2024-08-27 09:33:51 +0900

    Time: 1477 ms (11.52%), Space: 0B (100%) - LeetHub

--- 1641-countries-you-can-safely-invest-in/1641-countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/1641-countries-you-can-safely-invest-in.sql
@@ -1,9 +1,6 @@
-WITH avg AS (
-    SELECT AVG(duration) FROM Calls
-)
 SELECT c.name country
 FROM Country c
 INNER JOIN Person p ON LEFT(p.phone_number, 3) = c.country_code
 INNER JOIN Calls ca ON ca.caller_id = p.id OR ca.callee_id = p.id
 GROUP BY 1
-HAVING AVG(duration) > (SELECT * FROM avg)
+HAVING AVG(duration) > (SELECT AVG(duration) FROM Calls)
