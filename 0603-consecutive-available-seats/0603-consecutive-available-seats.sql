e5370574e63f725ad9ecbf3e3c9187bd4240a976
seokjin shin <sars21@hanmail.net>
2024-08-28 10:09:57 +0900

    Time: 333 ms (86.57%), Space: 0B (100%) - LeetHub

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -1,9 +1,11 @@
 WITH data AS (
-    SELECT seat_id 
-    FROM Cinema 
-    WHERE free   
-)
-SELECT *
-FROM data
-WHERE seat_id +1 IN (SELECT * FROM data)
-OR seat_id - 1 IN (SELECT * FROM data);
+    SELECT seat_id
+    FROM Cinema
+    WHERE free
+) 
+SELECT d1.seat_id 
+FROM data d1
+LEFT JOIN data d2 ON d1.seat_id = d2.seat_id - 1
+LEFT JOIN data d3 ON d1.seat_id = d3.seat_id + 1
+WHERE d2.seat_id IS NOT NULL 
+OR d3.seat_id IS NOT NULL
