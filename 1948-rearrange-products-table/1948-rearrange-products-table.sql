644cb42ac4cd6df81516791883816a56be3c19cc
seokjin shin <sars21@hanmail.net>
2024-08-28 11:10:10 +0900

    Time: 451 ms (88.71%), Space: 0B (100%) - LeetHub

---
+++ 1948-rearrange-products-table/1948-rearrange-products-table.sql
@@ -0,0 +1,12 @@
+
+SELECT product_id, "store1" store, store1 price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION ALL
+SELECT product_id, "store2" store, store2 price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION ALL
+SELECT product_id, "store3" store, store3 price
+FROM Products
+WHERE store3 IS NOT NULL;
