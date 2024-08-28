3fa2011f299ba7bb82158ed4c8657f9f1ea13f61
seokjin shin <sars21@hanmail.net>
2024-08-28 10:05:41 +0900

    Time: 371 ms (56.59%), Space: 0B (100%) - LeetHub

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -1,8 +1,9 @@
-SELECT seat_id FROM (
-  SELECT seat_id, free, 
-    LEAD(free,1,FALSE) OVER (ORDER BY seat_id) free_1,
-    LAG(free,1,FALSE) OVER (ORDER BY seat_id) free_2
-  FROM cinema
-) d
-WHERE free 
-AND (free_1 OR free_2)
+WITH data AS (
+    SELECT seat_id 
+    FROM Cinema 
+    WHERE free   
+)
+SELECT *
+FROM data
+WHERE seat_id +1 IN (SELECT * FROM data)
+OR seat_id - 1 IN (SELECT * FROM data);
