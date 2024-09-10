cb4d6ae812c03c01ce287ae13f427e76693f1c8d
seokjin shin <sars21@hanmail.net>
2024-09-10 20:29:12 +0900

    Time: 1000 ms (5.12%), Space: 0B (100%) - LeetHub

--- 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
@@ -1,9 +1,9 @@
 WITH max_amount AS (
-    SELECT DATE_FORMAT(day, "%Y-%m-%d") target_date, MAX(amount) amount
+    SELECT DATE(day) target_date, MAX(amount) amount
     FROM Transactions
     GROUP BY 1
 ) 
 SELECT t.transaction_id
 FROM Transactions t
-INNER JOIN max_amount m ON DATE_FORMAT(t.day, "%Y-%m-%d") = m.target_date AND t.amount = m.amount
+INNER JOIN max_amount m ON DATE(t.day) = m.target_date AND t.amount = m.amount
 ORDER BY 1
