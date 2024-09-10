1b63587c18829217d47e0c4dcf51d384e1a27a38
seokjin shin <sars21@hanmail.net>
2024-09-10 13:01:30 +0900

    Time: 2438 ms (31.9%), Space: 0B (100%) - LeetHub

---
+++ 1688-the-most-recent-orders-for-each-product/1688-the-most-recent-orders-for-each-product.sql
@@ -0,0 +1,7 @@
+SELECT product_name, product_id, order_id, order_date
+FROM (
+    SELECT p.product_name, o.product_id, order_id, o.order_date, RANK() OVER(PARTITION BY p.product_id ORDER BY o.order_date DESC) as num
+FROM Orders o
+INNER JOIN Products p ON o.product_id = p.product_id) sorted
+WHERE num = 1
+ORDER BY 1, 2, 3
