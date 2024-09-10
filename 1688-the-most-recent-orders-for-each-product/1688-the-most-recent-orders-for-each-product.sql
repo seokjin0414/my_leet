1d1a4cfa965beb04ba34973c7c7ddb460b4264e7
seokjin shin <sars21@hanmail.net>
2024-09-10 13:02:20 +0900

    Time: 1968 ms (84.42%), Space: 0B (100%) - LeetHub

--- 1688-the-most-recent-orders-for-each-product/1688-the-most-recent-orders-for-each-product.sql
+++ 1688-the-most-recent-orders-for-each-product/1688-the-most-recent-orders-for-each-product.sql
@@ -1,6 +1,6 @@
 SELECT product_name, product_id, order_id, order_date
 FROM (
-    SELECT p.product_name, o.product_id, order_id, o.order_date, RANK() OVER(PARTITION BY p.product_id ORDER BY o.order_date DESC) as num
+    SELECT p.product_name, o.product_id, order_id, o.order_date, RANK() OVER(PARTITION BY p.product_name, p.product_id ORDER BY o.order_date DESC) as num
 FROM Orders o
 INNER JOIN Products p ON o.product_id = p.product_id) sorted
 WHERE num = 1
