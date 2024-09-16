c769ed5321783c110cfd7319652cce5e9f4fe9a0
seokjin shin <sars21@hanmail.net>
2024-09-17 00:22:00 +0900

    Time: 540 ms (43.44%), Space: 0B (100%) - LeetHub

--- 1546-find-the-quiet-students-in-all-exams/1546-find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/1546-find-the-quiet-students-in-all-exams.sql
@@ -1,19 +1,22 @@
-WITH data AS(
-    SELECT s.student_id, s.student_name, e.exam_id, e.score, 
-    MAX(score) OVER(PARTITION BY exam_id ) max_score,
-    MIN(score) OVER(PARTITION BY exam_id ) min_score
-    FROM student s 
-    INNER JOIN exam e USING(student_id)
-),
-not_quiet AS (
-    SELECT student_id, student_name
-    FROM data 
-    WHERE score = max_score 
-    OR score = min_score
-    GROUP BY 1
-)
-SELECT student_id, student_name
-FROM data 
-WHERE student_id NOT IN (SELECT student_id FROM not_quiet)
-GROUP BY 1
-ORDER BY 1
+
+SELECT s.student_id, s.student_name
+FROM Student s
+JOIN (
+    SELECT a.student_id, COUNT(a.exam_id) AS total_exam,
+        SUM(
+            CASE
+                WHEN a.score > min_score AND a.score < max_score THEN 1
+                ELSE 0
+            END
+        ) AS quite_exam
+    FROM Exam a
+    JOIN (
+        SELECT exam_id, MIN(score) AS min_score, MAX(score) AS max_score
+        FROM Exam
+        GROUP BY exam_id
+    ) b
+    ON a.exam_id = b.exam_id
+    GROUP BY a.student_id
+) c
+ON s.student_id = c.student_id
+WHERE c.total_exam = c.quite_exam
