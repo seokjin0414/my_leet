263c512639074dd6761bdd8d78bb27b2de75ecfc
seokjin shin <sars21@hanmail.net>
2024-09-20 15:06:48 +0900

    Time: 1 ms (77.34%), Space: 2.2 MB (46.88%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.rs
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.rs
@@ -1,19 +1,21 @@
 impl Solution {
     pub fn gcd_of_strings(str1: String, str2: String) -> String {
-        if str1.clone() + &str2 == str2.clone() + &str1 {
-            let gcd_len = gcd(str1.len(), str2.len());
-            str1[..gcd_len].to_string()
-        } else {
-            "".to_string()
+        if str1 == str2 { return str1; }
+
+        let mut result:String = String::new();
+        let letters1 = str1.chars().collect::<Vec<char>>();
+        let letters2 = str2.chars().collect::<Vec<char>>();
+        
+        for word_length in 1..=str1.len().min(str2.len()) {
+            let substring = &letters1[..word_length];
+
+            if str1.len() % substring.len() != 0 || str2.len() % substring.len() != 0 { continue; }
+            if !letters1.chunks(substring.len()).all(|chunk| chunk == substring ) { continue; }
+            if !letters2.chunks(substring.len()).all(|chunk| chunk == substring ) { continue; }
+
+            result = substring.iter().collect::<String>();
         }
+        
+        result
     }
-}
-fn gcd(mut a: usize, mut b: usize) -> usize {
-    while b != 0 {
-        if b < a {
-            (a, b) = (b, a);
-        }
-        b %= a;
-    }
-    a
 }
