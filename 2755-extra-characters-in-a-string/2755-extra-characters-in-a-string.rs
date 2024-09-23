ba8676b07321ccc7a589da6625d8e615dbd8e1ac
seokjin shin <sars21@hanmail.net>
2024-09-23 19:42:36 +0900

    Time: 46 ms (18.18%), Space: 2.3 MB (9.09%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.rs
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.rs
@@ -1,16 +1,21 @@
+use std::collections::HashSet;
+
 impl Solution {
-  pub fn min_extra_char(s: String, dictionary: Vec<String>) -> i32 {
-    let mut res = vec![0; s.len() + 1];
+    pub fn min_extra_char(s: String, dictionary: Vec<String>) -> i32 {
+        let max = s.len() as i32 + 1;
+        let mut dp = vec![max; s.len() + 1];
+        dp[0] = 0;
 
-    for i in (0 .. s.len()).rev() {
-      res[i] = res[i + 1] + 1;
-      for w in &dictionary {
-        if (&s[i..]).starts_with(w) {
-          res[i] = res[i].min(res[i + w.len()]);
+        let dictionary_set: HashSet<_> = dictionary.into_iter().collect();
+
+        for i in 1..=s.len() {
+            dp[i] = dp[i - 1] + 1;
+            for l in 1..=i {
+                if dictionary_set.contains(&s[i-l..i].to_string()) {
+                    dp[i] = std::cmp::min(dp[i], dp[i-l]);
+                }
+            }
         }
-      }
+        dp[s.len()] as i32
     }
-
-    return res[0];
-  }
 }
