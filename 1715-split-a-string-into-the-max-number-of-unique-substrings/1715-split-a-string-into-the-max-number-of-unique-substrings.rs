162b1aa94981ded5e4bef08d2651c4f3ead97aa7
seokjin shin <sars21@hanmail.net>
2024-10-21 09:21:01 +0900

    Time: 53 ms (100%), Space: 2.1 MB (100%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.rs
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.rs
@@ -6,15 +6,15 @@ struct Sub {
 }
 
 impl Sub {
-  fn dfs(&mut self, set:&mut HashSet<Vec<char>>, ci:usize) {
-    let len = self.s.len();
-    if ci == len {
+  fn dfs(&mut self, set: &mut HashSet<Vec<char>>, size: usize) {
+    let l = self.s.len();
+    if size == l {
       self.result = std::cmp::max(self.result, set.len());
       return
     }
 
     let mut temp = vec![];
-    for i in ci..len {
+    for i in size..l {
       temp.push(self.s[i]);
 
       if !set.contains(&temp) {
@@ -28,8 +28,11 @@ impl Sub {
 
 impl Solution {
   pub fn max_unique_split(s: String) -> i32 {
-      let mut s = Sub { result:0, s:s.chars().collect() };
-      s.dfs(&mut HashSet::new(), 0);
-      s.result as i32
+    let mut s = Sub {
+        result: 0,
+        s: s.chars().collect()
+    };
+    s.dfs(&mut HashSet::new(), 0);
+    s.result as i32
   }
 }
