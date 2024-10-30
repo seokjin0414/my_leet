4fad15bc6011c81405850169ff6ff952ebf63ddc
seokjin shin <sars21@hanmail.net>
2024-10-30 17:13:51 +0900

    Time: 1 ms (100%), Space: 2.2 MB (100%) - LeetHub

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.rs
@@ -0,0 +1,38 @@
+impl Solution {
+    fn lis_length(v: &Vec<i32>) -> Vec<i32> {
+        let mut lis = vec![v[0]];
+        let mut lis_len = vec![1; v.len()];
+        
+        for i in 1..v.len() {
+            if v[i] > *lis.last().unwrap() {
+                lis.push(v[i]);
+            } else {
+                let index = lis.partition_point(|&x| x < v[i]);
+                lis[index] = v[i];
+            }
+            lis_len[i] = lis.len() as i32;
+        }
+        lis_len
+    }
+    
+    pub fn minimum_mountain_removals(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        
+        let lis = Self::lis_length(&nums);
+        
+        let mut reversed_nums = nums.clone();
+        reversed_nums.reverse();
+        let mut lds = Self::lis_length(&reversed_nums);
+        lds.reverse();
+        
+        let mut removals = i32::MAX;
+        
+        for i in 0..n {
+            if lis[i] > 1 && lds[i] > 1 {
+                removals = removals.min(n as i32 + 1 - lis[i] - lds[i]);
+            }
+        }
+        
+        removals
+    }
+}
