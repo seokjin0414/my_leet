d93612254e510d5dce4df813b4623a7586e8ca40
seokjin shin <sars21@hanmail.net>
2024-11-10 12:44:47 +0900

    Time: 18 ms (100%), Space: 4 MB (66.67%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.rs
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.rs
@@ -1,44 +1,36 @@
 impl Solution {
-  fn update_counts(cnts: &mut Vec<i32>, mut val: i32, delta: i32) {
-    let mut pos = 0;
-    while val != 0 {
-      if val % 2 == 1 {
-        cnts[pos] += delta;
-      }
-      val /= 2;
-      pos += 1;
-    }
-  }
+    pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
+        if k <= 1 { return 1 }
+        let (mut left, mut current_or, mut min_len) = (0, 0, i32::MAX);
+        let mut bit_counts = [0; 32];
 
-  fn counts_to_num(cnts: &Vec<i32>) -> i32 {
-    let (mut res, mut mult) = (0, 1);
-    for i in 0 .. 30 {
-      if cnts[i] > 0 {
-        res += mult;
-      }
-      mult *= 2;
-    }
-    return res
-  }
+        for (right, &num) in (1..).zip(nums.iter()) {
+            if num >= k { return 1 }
+            current_or |= num;
+            // Update the count of bits that have been set in the current_or number.
+            (0..32).for_each(|bit| bit_counts[bit as usize] += (num >> bit & 1 == 1) as i32);
 
-  pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
-    if k == 0 {
-      return 1
-    }
-    
-    let (mut l, mut res, mut cnt) = (0, usize::MAX, vec![0; 30]);
-    for r in 0 .. nums.len() {
-      Self::update_counts(&mut cnt, nums[r], 1);
-      while Self::counts_to_num(&cnt) >= k {
-        res = res.min(r - l + 1);
-        Self::update_counts(&mut cnt, nums[l], -1);
-        l += 1;
-      }
-    }
+            while current_or >= k {
+                // Update the length of min_array if possible.
+                min_len = min_len.min(right - left);
+                // Decrease bits_counts for bits present in left num.
+                (0..32).for_each(|bit| {
+                    let to_remove = nums[left as usize];
+                    let decrement = to_remove >> bit & 1 == 1;
+                    // If any decrement to zero, remove that bit from the current_or.
+                    if decrement && bit_counts[bit as usize] == 1 {
+                        current_or ^= 1 << bit
+                    }
+                    bit_counts[bit as usize] -= decrement as i32;
+                });
 
-    if res == usize::MAX {
-      return -1
+                left += 1;
+            }
+        }
+
+        match min_len {
+            i32::MAX => -1,
+            _ => min_len
+        }
     }
-    return res as i32
-  }
 }
