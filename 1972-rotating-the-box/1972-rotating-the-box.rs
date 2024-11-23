9a1a7c792e26dbbd37580785a2f60ef78ed72557
seokjin shin <sars21@hanmail.net>
2024-11-23 21:19:27 +0900

    Time: 134 ms (100%), Space: 18.9 MB (50%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.rs
+++ 1972-rotating-the-box/1972-rotating-the-box.rs
@@ -1,28 +1,22 @@
 impl Solution {
-    pub fn rotate_the_box(mybox: Vec<Vec<char>>) -> Vec<Vec<char>> {
-        let m = mybox.len();
-        let n = mybox[0].len();
-        let mut rotated = vec![vec!['.'; m]; n];
-
-        // Rotate the box 90 degrees clockwise
-        for (row_idx, row) in mybox.iter().enumerate() {
-            let mut empty_row = n;
-            for (col_idx, &cell) in row.iter().enumerate().rev() {
-                match &cell {
-                    '#' => {
-                        empty_row -= 1;
-                        rotated[empty_row][m - row_idx - 1] = cell;
+    pub fn rotate_the_box(mut bx: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = vec![vec!['.';bx.len()];bx[0].len()];
+        for row in 0..bx.len() {
+            let mut bottom = bx[row].len()-1;
+            for col in (0..bx[row].len()).rev() {
+                if bx[row][col] == '*' { bottom = col-1; }
+                if bx[row][col] == '#' {
+                    if bottom != col {
+                        (bx[row][bottom],bx[row][col]) = ('#','.');
+                        result[bottom][bx.len()-row-1] = '#';
+                        bottom -= 1;
+                    } else {
+                        bottom = col-1;
                     }
-                    '*' => {
-                        rotated[col_idx][m - row_idx - 1] = '*';
-                        empty_row = col_idx;
-                    }
-                    _ => {}
                 }
+                result[col][bx.len()-row-1] = bx[row][col];                                
             }
         }
-
-
-        rotated
+        result
     }
 }
