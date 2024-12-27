25d471d4e1be2d842991ffced5892fd8dc8d21e4
seokjin shin <sars21@hanmail.net>
2024-12-27 16:38:38 +0900

    Time: 2669 ms (5.31%), Space: 0B (100%) - LeetHub

---
+++ 2014-orders-with-maximum-quantity-above-average/2014-orders-with-maximum-quantity-above-average.sql
@@ -0,0 +1,11 @@
+WITH cal AS (
+    SELECT 
+        order_id, 
+        MAX(quantity) max_v, 
+        SUM(quantity) / COUNT(product_id) avg_v
+    FROM OrdersDetails
+    GROUP BY 1
+)
+SELECT order_id
+FROM cal
+WHERE max_v > (Select MAX(avg_v) FROM cal)
