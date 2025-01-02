f6af23df8fc02b0cb17da00c953700b6826b8540
seokjin shin <sars21@hanmail.net>
2025-01-02 16:36:20 +0900

    Time: 448 ms (72.3%), Space: 0B (100%) - LeetHub

--- 0614-second-degree-follower/0614-second-degree-follower.sql
+++ 0614-second-degree-follower/0614-second-degree-follower.sql
@@ -1,6 +1,14 @@
-SELECT followee follower, COUNT(follower) num
-FROM Follow
-WHERE followee IN (SELECT followee FROM Follow)
-AND followee IN (SELECT follower FROM Follow)
-GROUP BY 1
-ORDER BY 1
+WITH wee AS (
+    SELECT followee, COUNT(*) num
+    FROM Follow
+    GROUP BY 1
+),
+wer AS (
+    SELECT follower
+    FROM Follow
+    GROUP BY follower
+)
+SELECT e.followee follower, num
+FROM wee e
+INNER JOIN wer r ON e.followee = r.follower
+ORDER BY 1;
