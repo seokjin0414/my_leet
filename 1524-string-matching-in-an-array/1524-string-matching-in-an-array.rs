5f7cf6cd084fd63480cb642890b1e3509f23cc4e
seokjin shin <sars21@hanmail.net>
2025-01-07 21:09:15 +0900

    Time: 4 ms (0%), Space: 2.2 MB (85.71%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.rs
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.rs
@@ -2,9 +2,11 @@ use std::collections::HashSet;
 impl Solution {
     pub fn string_matching(words: Vec<String>) -> Vec<String> {
         let mut result = HashSet::new();
+        
         for pattern in &words {
             for word in &words {
                 if word == pattern { continue }
+                
                 if word.find(pattern).is_some() {
                     result.insert(pattern.into());
                 }
