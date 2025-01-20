6035cd4fb91d8e6d68c006b56449fc1b645ac747
seokjin shin <sars21@hanmail.net>
2025-01-20 13:41:37 +0900

    Time: 0 ms (100%), Space: 5.1 MB (83.33%) - LeetHub

--- 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.rs
+++ 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.rs
@@ -1,79 +1,45 @@
-use std::collections::{VecDeque, HashSet};
-
-#[derive(Debug, Copy,Clone,PartialEq, Eq, Hash)]
-struct Location(usize,usize);
-
-fn find_person(map : &Vec<Vec<char>>) -> Location {
-    for (i,&ref arr) in map.iter().enumerate() {
-        for (j, &c) in arr.iter().enumerate() {
-            if c == '*' {
-                return Location(i,j);
-            }
-        }
-    }
-    panic!("No * in input map!!!");
-}
-
-fn find_food(map : &Vec<Vec<char>>) -> HashSet<Location> {
-    let mut solution : HashSet<Location> = HashSet::new();
-    for (i,&ref arr) in map.iter().enumerate() {
-        for (j, &c) in arr.iter().enumerate() {
-            if c == '#' {
-                solution.insert(Location(i,j));
-            }
-        }
-    }
-    return solution;
-}
+use std::collections::VecDeque;
 
 impl Solution {
+
     pub fn get_food(grid: Vec<Vec<char>>) -> i32 {
-        let you : Location = find_person(&grid);
-        let food: HashSet<Location> = find_food(&grid);
-        let mut queue: VecDeque<(Location, i32)> = VecDeque::new();
-        let mut in_queue : HashSet<Location> = HashSet::new();
-        let mut visited : HashSet<Location> = HashSet::new();
+        const VISITED: char = 'V';
+        let mut grid = grid;
+        let (num_rows, num_cols) = (grid.len() as i32, grid[0].len() as i32);
+        let mut start: (usize, usize) = (0, 0);
 
-        queue.push_back((you, 0));
-        in_queue.insert(you);
-        visited.insert(you);
-
-        let m = grid.len() - 1;
-        let n = grid[0].len() - 1;
-
-        while !queue.is_empty() {
-            let (current, dist_travelled) = queue.pop_front().unwrap();
-            match grid[current.0][current.1] {
-                '#' => return dist_travelled,
-                'X' => continue, 
-                'O' | '*' => {
-                    visited.insert(current);
-                    let l : Location = Location(current.0,current.1.saturating_sub(1));
-                    let r : Location = Location(current.0,current.1 + 1);
-                    let u : Location = Location(current.0.saturating_sub(1),current.1);
-                    let d : Location = Location(current.0 + 1, current.1);
-                  
-                    if current.1 > 0 && !visited.contains(&l) && !in_queue.contains(&l) { 
-                        queue.push_back((l, dist_travelled + 1));
-                        in_queue.insert(l);
-                    }
-                    if current.1 < n && !visited.contains(&r) && !in_queue.contains(&r) { 
-                        queue.push_back((r, dist_travelled + 1));
-                        in_queue.insert(r);    
-                    }
-                    if current.0 > 0 && !visited.contains(&u) && !in_queue.contains(&u) {
-                        queue.push_back((u, dist_travelled + 1)); 
-                        in_queue.insert(u);
-                    }
-                    if current.0 < m && !visited.contains(&d) && !in_queue.contains(&d) {
-                        queue.push_back((d, dist_travelled + 1)); 
-                        in_queue.insert(d);    
-                    }
-                },
-                _ => panic!("Should never hit this condition"),
+        for (r, row) in grid.iter().enumerate() {
+            for (c, ch) in row.iter().enumerate() {
+                if *ch == '*' {
+                    start = (r, c);
+                }
             }
         }
 
-        return -1;
+        let mut q = VecDeque::new();
+        q.push_back(((start.0, start.1), 0i32));
+        grid[start.0][start.1] = VISITED;
+
+        while let Some(pos) = q.pop_front() {
+            let (row, col, dist) = (pos.0.0, pos.0.1, pos.1);
+            for delta in [0, 1, 0, -1, 0].windows(2) {
+                let new_row = delta[0] + row as i32;
+                let new_col = delta[1] + col as i32;
+
+                if (0..num_rows).contains(&new_row) && (0..num_cols).contains(&new_col) {
+                    let (new_row, new_col) = (new_row as usize, new_col as usize);
+                    if grid[new_row][new_col] == '#' {
+                        return dist + 1;
+                    }
+            
+                    if grid[new_row][new_col] == 'O' {
+                        q.push_back(((new_row, new_col), dist + 1));
+                        grid[new_row][new_col] = VISITED;
+                    }
+                }
+            }
+        }
+
+        -1
     }
 }
