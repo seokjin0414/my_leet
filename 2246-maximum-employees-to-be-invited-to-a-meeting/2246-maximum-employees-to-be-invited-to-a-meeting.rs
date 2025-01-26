7dc57583acff3db1e13a33df52aad115d9ecb128
seokjin shin <sars21@hanmail.net>
2025-01-26 14:23:46 +0900

    Time: 4 ms (100%), Space: 3.6 MB (100%) - LeetHub

--- 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.rs
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.rs
@@ -1,54 +1,47 @@
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn maximum_invitations(favorite: Vec<i32>) -> i32 {
         let n = favorite.len();
-        let mut visited = vec![false; n];
-        let mut to_leaf = vec![0; n];
-        let mut in_degree = vec![0; n];
-
-        for &fav in favorite.iter() {
-            in_degree[fav as usize] += 1;
+        let mut dgree = vec![0;n];
+        let mut dep = vec![0;n];
+        for &p in favorite.iter(){
+            dgree[p as usize] += 1;
         }
-
-        let mut queue = VecDeque::new();
-        for (i, &deg) in in_degree.iter().enumerate() {
-            if deg == 0 {
-                queue.push_back(i);
-                visited[i] = true;
+        let mut q = vec![];
+        for i in 0..n{
+            if dgree[i] == 0 {
+                q.push(i as usize);
             }
         }
+        while q.len() !=0 {
+            let x = q.pop().unwrap();
+            let y = favorite[x as usize] as usize;
+            dgree[y] -=1 ;
+            dep[y] = dep[y].max(dep[x]+1);
 
-        while let Some(node) = queue.pop_front() {
-            let next = favorite[node] as usize;
-            to_leaf[next] = to_leaf[next].max(1 + to_leaf[node]);
-            in_degree[next] -= 1;
-            if in_degree[next] == 0 {
-                visited[next] = true;
-                queue.push_back(next);
+            if dgree[y] == 0{
+                q.push(y);
             }
         }
-
-        let (mut max_cycle, mut pairs_with_chains) = (0, 0);
-        for i in 0..n {
-            if !visited[i] {
-                let mut len = 0;
-                let mut curr = i;
-
-                while !visited[curr] {
-                    visited[curr] = true;
-                    len += 1;
-                    curr = favorite[curr] as usize;
+        let mut ans = 0;
+        let mut two = 0; 
+        for i in 0..n{
+            if dgree[i] != 0 {
+                dgree[i] = 0;
+                let mut j = favorite[i] as usize;
+                let mut tmp = 1;
+                while dgree[j]!=0{
+                    tmp+=1;
+                    dgree[j]=0;
+                    j = favorite[j] as usize;
                 }
-
-                if len == 2 {
-                    pairs_with_chains += 2 + to_leaf[i] + to_leaf[favorite[i] as usize];
-                } else {
-                    max_cycle = max_cycle.max(len);
+                if tmp == 2 {
+                    two += (2 + dep[i] + dep[favorite[i] as usize]);
+                }else {
+                    ans = ans.max(tmp);
                 }
             }
         }
-
-        max_cycle.max(pairs_with_chains)
+        ans = ans.max(two);
+        ans
     }
 }
