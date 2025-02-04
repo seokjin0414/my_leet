e134854e1048638161daf4624d646a49e2883ad2
seokjin shin <sars21@hanmail.net>
2025-02-04 15:25:42 +0900

    Time: 0 ms (100%), Space: 2.4 MB (9.09%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.rs
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.rs
@@ -1,12 +1,5 @@
 impl Solution {
-    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {        
-        let (mut prev,mut result,mut sum) = (nums[0],nums[0],nums[0]);
-        for &num in nums.iter().skip(1) {
-            if num <= prev { sum = 0; }
-            sum += num;
-            result = result.max(sum);
-            prev = num;
-        }
-        result
+    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+        nums.chunk_by(|a, b| b > a).map(|c| c.iter().sum()).max().unwrap()
     }
 }
