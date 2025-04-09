810a356bbc327003619c92be1b1eb3f642e3d26d
seokjin shin <sars21@hanmail.net>
2025-04-09 23:53:10 +0900

    Time: 0 ms (100%), Space: 2.4 MB (88.89%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.rs
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.rs
@@ -1,31 +1,53 @@
-use std::collections::{HashSet, VecDeque};
+pub struct UnionFind{
+    pub parent: Vec<i32>,
+    pub rank: Vec<i32>,
+}
+
+impl UnionFind{
+    pub fn new(n: usize) -> Self{
+        Self{
+            parent: (0..n as i32).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    pub fn find(&mut self, x: i32) -> i32{
+        if self.parent[x as usize] != x{
+            self.parent[x as usize] = self.find(self.parent[x as usize])
+        }
+        self.parent[x as usize]
+    }
+
+    pub fn union_set(&mut self, x: i32, y:i32, count: &mut i32) {
+        let x_parent = self.find(x) as usize;
+        let y_parent = self.find(y) as usize;
+
+        if x_parent == y_parent{
+            return;
+        }
+
+        *count -= 1;
+        if self.rank[x_parent] < self.rank[y_parent]{
+            self.parent[x_parent] = self.parent[y_parent];
+        } else if self.rank[x_parent] > self.rank[y_parent]{
+            self.parent[y_parent] = self.parent[x_parent];
+        }else{
+            self.parent[y_parent] = self.parent[x_parent];
+            self.rank[x_parent] += 1
+        }
+    }
+}
 
 impl Solution {
     pub fn count_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-        let n = n as usize;
-        let adjacency_list = {
-            let mut res = vec![vec![]; n];
-            for edge in edges {
-                res[edge[0] as usize].push(edge[1] as usize);
-                res[edge[1] as usize].push(edge[0] as usize);
-            }
-            res
-        };
-        let mut res = 0;
-        let mut visited = HashSet::new();
-        for i in 0..n {
-            if visited.contains(&i) { continue; }
-            res += 1;
-            let mut queue = VecDeque::new();
-            queue.push_back(i);
-            while let Some(node) = queue.pop_front() {
-                visited.insert(node);
-                for &idx in adjacency_list[node].iter() {
-                    if visited.contains(&idx) { continue; }
-                    queue.push_back(idx);
-                }
-            }
+        let mut uf = UnionFind::new(n as usize);
+
+        let mut count = n;
+
+        for edge in edges{
+            uf.union_set(edge[0], edge[1], &mut count);
         }
-        res
+
+        count
     }
 }
