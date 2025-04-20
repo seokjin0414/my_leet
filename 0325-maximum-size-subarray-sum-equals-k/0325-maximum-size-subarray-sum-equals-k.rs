4e13cc5bfdd5e7cadc331513c95904adc1ba5133
seokjin shin <sars21@hanmail.net>
2025-04-20 22:39:08 +0900

    Time: 31 ms (73.08%), Space: 7.8 MB (69.23%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.rs
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.rs
@@ -2,20 +2,28 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn max_sub_array_len(nums: Vec<i32>, k: i32) -> i32 {
-        let m = nums.len();                             // Length of nums
-        let mut map : HashMap<i32,i32> = HashMap::new();// Hashmap that maps sum to leftmost appearance in sum_left
-        let mut ret = 0;                                
-        let mut total : i32 = 0;
-        for (i,&num) in nums.iter().enumerate().rev() {
-            total += num;
-            if let Some(value) = map.get(&(total - k)) {
-                ret = std::cmp::max(*value - i as i32, ret);
-            }
-            if total == k {
-                ret = (m - i) as i32;
-            }
-            map.entry(total).or_insert(i as i32);
-        }
-        return ret;
+        let mut longest = 0;
+        let mut map = [(0, -1)].into_iter().collect::<HashMap<i32,i32>>();
+        nums
+            .into_iter()
+            .enumerate()
+            .fold(0, |mut sum, (index, num)| {
+                sum += num;
+
+                if num == k {
+                    longest = longest.max(1);
+                }
+                match map.get(&(sum - k)) {
+                    Some(previous_index) => {
+                        longest = longest.max(index as i32 - previous_index);
+                    },
+                    None => (),
+                }
+                map.entry(sum).or_insert(index as i32);
+
+                sum
+            });
+
+        longest as i32
     }
 }
