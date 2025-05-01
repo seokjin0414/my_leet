da8392f82bee2782e60402350732914b09f6881a
seokjin shin <sars21@hanmail.net>
2025-05-01 21:19:07 +0900

    Time: 31 ms (33.33%), Space: 4.9 MB (66.67%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.rs
+++ 0353-design-snake-game/0353-design-snake-game.rs
@@ -1,69 +1,66 @@
-use std::collections::VecDeque;
-use std::collections::HashSet;
-
-#[derive(PartialEq, Eq, Hash, Debug)]
-struct Pos {
-  y: i32,
-  x: i32,
-}
+use std::collections::{HashMap, HashSet, VecDeque};
 
 struct SnakeGame {
-  Y: i32,
-  X: i32,
-  snake_deque: VecDeque<Pos>,
-  snake_set: HashSet<Pos>,
-  food: Vec<Pos>,
-  food_pos: usize,
+    width: i32,
+    height: i32,
+    food: Vec<(i32, i32)>,
+    food_index: usize,
+    directions: HashMap<char, (i32, i32)>,
+    snake_set: HashSet<(i32, i32)>,
+    snake: VecDeque<(i32, i32)>,
 }
 
 impl SnakeGame {
 
-  fn new(width: i32, height: i32, food: Vec<Vec<i32>>) -> Self {
-    let mut food_data = Vec::with_capacity(food.len() + 1);
-    for el in food {
-      food_data.push(Pos{y: el[0], x: el[1]});
-    }
-    food_data.push(Pos{y: -1, x: -1});
+    fn new(width: i32, height: i32, food: Vec<Vec<i32>>) -> Self {
+       let mut directions = HashMap::new();
+        directions.insert('L', (0, -1));
+        directions.insert('R', (0, 1));
+        directions.insert('U', (-1, 0));
+        directions.insert('D', (1, 0));
 
-    return SnakeGame{
-      Y: height,
-      X: width,
-      snake_deque: VecDeque::from([Pos{y: 0, x: 0}]),
-      snake_set: HashSet::from([Pos{y: 0, x: 0}]),
-      food: food_data,
-      food_pos: 0,
-    }
-  }
-  
-  fn make_a_move(&mut self, direction: String) -> i32 {
-    let p = self.snake_deque.front().unwrap_or(&Pos{y:-1, x:-1});
-    let (mut y, mut x) = (p.y, p.x);
-    if direction == "U" {
-      y -= 1;
-    } else if direction == "R" {
-      x += 1;
-    } else if direction == "D" {
-      y += 1;
-    } else {
-      x -= 1;
-    }
+        let mut snake = VecDeque::new();
+        snake.push_front((0, 0));
 
-    if y < 0 || x < 0 || y == self.Y || x == self.X {
-      return -1;
-    }
+        let mut snake_set = HashSet::new();
+        snake_set.insert((0, 0));
 
-    self.snake_deque.push_front(Pos{y: y, x: x});
-    if y == self.food[self.food_pos].y && x == self.food[self.food_pos].x {
-      self.food_pos += 1;
-    } else {
-      self.snake_set.remove(&self.snake_deque.pop_back().unwrap_or(Pos{y:-1, x:-1}));
+        SnakeGame{
+            width,
+            height,
+            food: food.into_iter().map(|f| (f[0], f[1])).collect(),
+            food_index: 0,
+            directions,
+            snake_set,
+            snake,
+        }
     }
+    
+    fn make_a_move(&mut self, direction: String) -> i32 {
+        let direction_char = direction.chars().next().unwrap();
+        let (dx, dy) = self.directions[&direction_char];
+        let (head_x, head_y) = self.snake.front().unwrap();
+        let new_head = (head_x + dx, head_y + dy);
 
-    if self.snake_set.contains(&Pos{y: y, x: x}) {
-      return -1;
+        let out_of_bounds = new_head.0 < 0 || new_head.0 >= self.height || new_head.1 < 0 || new_head.1 >= self.width;
+        let bites_self = self.snake_set.contains(&new_head) && * self.snake.back().unwrap() != new_head;
+
+        if bites_self || out_of_bounds {
+            return -1;
+        }
+
+        let eat_food = self.food_index < self.food.len() && self.food[self.food_index] == new_head;
+
+        if eat_food {
+            self.food_index += 1;
+        } else {
+            let tail = self.snake.pop_back().unwrap();
+            self.snake_set.remove(&tail);
+        }
+
+        self.snake.push_front(new_head);
+        self.snake_set.insert(new_head);
+
+        (self.snake.len() as i32) - 1
     }
-
-    self.snake_set.insert(Pos{y: y, x: x});
-    return (self.snake_deque.len() - 1) as i32;
-  }
 }
