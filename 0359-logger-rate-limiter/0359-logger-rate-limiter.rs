ecd165489aa497637d00b1441b9b77bc2188b6ad
seokjin shin <sars21@hanmail.net>
2025-05-11 17:38:26 +0900

    Time: 11 ms (11.11%), Space: 8.4 MB (94.44%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.rs
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.rs
@@ -1,36 +1,22 @@
-use std::collections::{HashSet, VecDeque};
-
+use std::collections::HashMap;
+#[derive(Debug, Default)]
 struct Logger {
-    msg_set: HashSet<String>,
-    msg_queue: VecDeque<(String, i32)>,
+    map: HashMap<String, i32>,
 }
 
 impl Logger {
 
     fn new() -> Self {
-        Self {
-            msg_set: HashSet::new(),
-            msg_queue: VecDeque::new(),
-        }
+        Default::default()
     }
     
     fn should_print_message(&mut self, timestamp: i32, message: String) -> bool {
-        while let Some((msg, ts)) = self.msg_queue.front() {
-            if timestamp - ts >= 10 {
-                if let Some((old_msg, _)) = self.msg_queue.pop_front() {
-                    self.msg_set.remove(&old_msg);
-                }
-            } else {
-                break;
+        if let Some(&v) = self.map.get(&message) {
+            if timestamp < v {
+                return false;
             }
         }
-
-        if !self.msg_set.contains(&message) {
-            self.msg_set.insert(message.clone());
-            self.msg_queue.push_back((message, timestamp));
-            true
-        } else {
-            false
-        }
+        self.map.insert(message, timestamp + 10);
+        true
     }
 }
