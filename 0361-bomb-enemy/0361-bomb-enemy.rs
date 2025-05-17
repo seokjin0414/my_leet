088a457295c9158ba61cb3db95fbba1cb9c3e003
seokjin shin <sars21@hanmail.net>
2025-05-17 23:59:57 +0900

    Time: 7 ms (75%), Space: 17.9 MB (50%) - LeetHub

--- 0361-bomb-enemy/0361-bomb-enemy.rs
+++ 0361-bomb-enemy/0361-bomb-enemy.rs
@@ -1,48 +1,42 @@
 impl Solution {
     pub fn max_killed_enemies(grid: Vec<Vec<char>>) -> i32 {
-        let (m, n) = (grid.len(), grid[0].len());
-        let mut dp =  vec![vec![0; n]; m];
-        
-        for i in 0 .. m {
-            let mut sk = vec![];
-            let mut cnt = 0;
-            
-            for j in 0 .. n {
-                if grid[i][j] == '0' { sk.push(j); }
-                if grid[i][j] == 'E' { cnt += 1; }
-                
-                if grid[i][j] == 'W' || j == n - 1 {
-                    while let Some(k) = sk.pop() {
-                        dp[i][k] += cnt; 
+        let m: usize = grid.len();
+        let n: usize = grid[0].len();
+
+        let mut max_count: i32 = 0;
+        let mut row_hits: i32 = 0;
+        let mut col_hits: Vec<i32> = vec![0; n];
+
+        for i in 0..m {
+            for j in 0..n {
+                if j == 0 || grid[i][j - 1] == 'W' {
+                    row_hits = 0;
+                    for k in j..n {
+                        if grid[i][k] == 'W' {
+                            break;
+                        } else if grid[i][k] == 'E' {
+                            row_hits += 1;
+                        }
                     }
-                    cnt = 0;
+                }
+
+                if i == 0 || grid[i - 1][j] == 'W' {
+                    col_hits[j] = 0;
+                    for k in i..m {
+                        if grid[k][j] == 'W' {
+                            break;
+                        } else if grid[k][j] == 'E' {
+                            col_hits[j] += 1;
+                        }
+                    }
+                }
+
+                if grid[i][j] == '0' {
+                    max_count = std::cmp::max(max_count, row_hits + col_hits[j]);
                 }
             }
         }
-        
-        for j in 0 .. n {
-            let mut sk = vec![];
-            let mut cnt = 0;
-            
-            for i in 0 .. m {
-                if grid[i][j] == '0' { sk.push(i); }
-                if grid[i][j] == 'E' { cnt += 1; }
-                
-                if grid[i][j] == 'W' || i == m - 1 {
-                    while let Some(k) = sk.pop() {
-                        dp[k][j] += cnt; 
-                    }
-                    cnt = 0;
-                }
-            }
-        }
-        
-        let mut ret = 0;
-        
-        for d in dp {
-            ret = ret.max(d.into_iter().max().unwrap());
-        }
-        
-        ret
+
+        max_count
     }
 }
