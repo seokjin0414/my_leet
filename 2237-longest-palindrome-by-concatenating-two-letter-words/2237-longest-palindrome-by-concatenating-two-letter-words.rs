c30ccde5a7ea961650eeae1ea279210213091927
seokjin shin <sars21@hanmail.net>
2025-05-25 22:50:59 +0900

    Time: 15 ms (75%), Space: 8.7 MB (100%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.rs
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.rs
@@ -1,40 +1,27 @@
-use std::collections::HashMap;
-
 impl Solution {
-  pub fn longest_palindrome(words: Vec<String>) -> i32 {
-    let mut hash: HashMap<String, i32> = HashMap::new();
-    let mut is_center: bool = false;
-    let mut longest_length = 0;
-  
-    for word in &words {
-      *hash.entry(word.clone()).or_insert(0) += 1;
-    }
-  
-    for (word, count) in hash.clone() {
-      if count == 0 {
-        continue;
-      }
-      let rev: String = word.chars().rev().collect();
-
-      if word == rev {
-        let pair = count / 2;
-        longest_length += 4 * pair;
-        if count % 2 == 1 {
-          is_center = true;
+    pub fn longest_palindrome(words: Vec<String>) -> i32 {
+        let mut open = [0; 26 * 26];
+        let mut total = 0;
+        for word in words {
+            let a = word.as_bytes()[0] as usize - 'a' as usize;
+            let b = word.as_bytes()[1] as usize - 'a' as usize;
+            let hash = a * 26 + b;
+            let inv_hash = b * 26 + a;
+            
+            if open[inv_hash] > 0 {
+                open[inv_hash] -= 1;
+                total += 4;
+            } else {
+                open[hash] += 1;
+            }
         }
-      } else if hash.contains_key(&rev) {
-        let min_pair = count.min(*hash.get(&rev).unwrap());
+        for i in 0..26 {
+            if open[i * 26 + i] > 0 {
+                total += 2;
+                break;
+            }
+        }
 
-        longest_length += min_pair * 4;
-
-        hash.insert(word.clone(), 0);
-        hash.insert(rev.clone(), 0);
-      }
+        total
     }
-    if is_center {
-      longest_length += 2;
-    }
-
-    longest_length 
-  }
 }
