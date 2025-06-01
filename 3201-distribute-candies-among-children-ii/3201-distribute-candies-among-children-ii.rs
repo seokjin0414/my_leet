309ea60f33fce9871c9d60ef137563e12e72866b
seokjin shin <sars21@hanmail.net>
2025-06-01 21:54:12 +0900

    Time: 2 ms (60%), Space: 2.1 MB (100%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.rs
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.rs
@@ -1,7 +1,13 @@
 impl Solution {
     pub fn distribute_candies(n: i32, limit: i32) -> i64 {
-        let n = n as i64;
-        let l = limit as i64;
-        (0..=n.min(l)).map(|i| 0.max(1 + l.min(n-i) - 0.max(n-l-i))).sum()
+        Self::cal(n + 2) - 3 * Self::cal(n - limit + 1) + 3 * Self::cal(n - (limit + 1) * 2 + 2) - Self::cal(n - 3 * (limit + 1) + 2)
+    }
+
+    fn cal(x: i32) -> i64 {
+        if x < 0 {
+            0
+        } else {
+            x as i64 * (x - 1) as i64 / 2
+        }
     }
 }
