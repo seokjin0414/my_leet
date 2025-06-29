db42c3fe88951afd59234abd6e0d78232e88ff98
seokjin shin <sars21@hanmail.net>
2025-06-30 00:17:01 +0900

    Time: 6 ms (88.89%), Space: 3.9 MB (22.22%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.rs
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.rs
@@ -1,28 +1,42 @@
+const MOD: u64 = 1000000007;
+
+const POW_2: [u32; 100001] = {
+    let mut pow_2 = [1; 100001];
+    let mut i = 0;
+    while i < 100000 {
+        pow_2[i + 1] = ((pow_2[i] as u64 * 2) % MOD) as u32;
+        i += 1;
+    }
+    pow_2
+};
+
 impl Solution {
-    pub fn num_subseq(mut nums: Vec<i32>, target: i32) -> i32 {
-        const MOD: i32 = 1_000_000_007;
-        let n = nums.len();
+    pub fn num_subseq(nums: Vec<i32>, target: i32) -> i32 {
+        let min = nums.iter().min().unwrap();
+        let mut nums: Vec<i32> = nums
+            .iter()
+            .filter(|&num| *num <= target - min)
+            .cloned()
+            .collect();
         nums.sort();
-
-        let mut power = vec![1; n];
-        for i in 1..n {
-            power[i] = (power[i - 1] * 2) % MOD;
-        }
-
-        let mut left = 0;
-        let mut right = n - 1;
-        let mut result = 0;
-
-        while left <= right {
-            if nums[left] + nums[right] <= target {
-                result = (result + power[right - left]) % MOD;
-                left += 1;
-            } else {
-                if right == 0 { break; }
-                right -= 1;
+        if nums.is_empty() {
+            0
+        } else {
+            let mut j = nums.len() - 1;
+            let mut ret = POW_2[j] as u64;
+            'outer: for (i, num) in nums.iter().enumerate().skip(1) {
+                if i > j {
+                    break 'outer;
+                }
+                while num + nums[j] > target {
+                    j -= 1;
+                    if i > j {
+                        break 'outer;
+                    }
+                }
+                ret += POW_2[j - i] as u64;
             }
+            (ret % MOD) as i32
         }
-
-        result
     }
 }
