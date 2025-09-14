f5d9c27dab2234c48b74ea4f23d8a4b3812490c7
seokjin shin <sars21@hanmail.net>
2025-09-14 23:25:39 +0900

    Time: 0 ms (100%), Space: 2 MB (100%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.rs
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.rs
@@ -1,18 +1,18 @@
+
 impl Solution {
     pub fn max_freq_sum(s: String) -> i32 {
-        let mut vowels = [0;26];
-        let mut consonants = [0;26];
-        fn is_vowel(b: u8) -> bool {
-            b"aeiou".contains(&b)
-        }
-        for b in s.bytes() {
-            if is_vowel(b) {
-                vowels[(b-b'a') as usize] +=1
-            }
-            else {
-                consonants[(b-b'a') as usize] +=1
+        let mut vowel_frequencies = [0; 26];
+        let mut consonant_frequencies = [0; 26];
+
+        for char in s.bytes() {
+            if char == b'a' || char == b'e' || char == b'i' || char == b'o' || char == b'u' {
+                vowel_frequencies[(char - b'a') as usize] += 1;
+            } else {
+                consonant_frequencies[(char - b'a') as usize] += 1;
             }
         }
-        vowels.into_iter().max().unwrap_or(0) + consonants.into_iter().max().unwrap_or(0)
+
+        vowel_frequencies.into_iter().max().unwrap_or(0)
+            + consonant_frequencies.into_iter().max().unwrap_or(0)
     }
 }
