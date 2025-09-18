9d098fb252d0b9049b34e4cb40df82fa5f8d9d1e
seokjin shin <sars21@hanmail.net>
2025-09-18 23:54:46 +0900

    Time: 50 ms (100%), Space: 46.4 MB (12.5%) - LeetHub

--- 3678-design-task-manager/3678-design-task-manager.rs
+++ 3678-design-task-manager/3678-design-task-manager.rs
@@ -2,62 +2,68 @@ use std::collections::{BinaryHeap, HashMap};
 use std::cmp::Ordering;
 
 #[derive(Eq, PartialEq)]
-struct Entry {
+struct Task {
     priority: i32,
     task_id: i32,
 }
 
-impl Ord for Entry {
+impl Ord for Task {
     fn cmp(&self, other: &Self) -> Ordering {
         self.priority.cmp(&other.priority)
-            .then(self.task_id.cmp(&other.task_id))
+            .then_with(|| self.task_id.cmp(&other.task_id))
     }
 }
-impl PartialOrd for Entry {
+
+impl PartialOrd for Task {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
 struct TaskManager {
-    heap: BinaryHeap<Entry>,
-    map: HashMap<i32, (i32, i32)>, // task_id -> (user_id, priority)
+    task_info: HashMap<i32, (i32, i32)>,
+    heap: BinaryHeap<Task>,
 }
 
 impl TaskManager {
     fn new(tasks: Vec<Vec<i32>>) -> Self {
-        let mut tm = TaskManager {
-            heap: BinaryHeap::new(),
-            map: HashMap::new(),
-        };
-        for t in tasks {
-            tm.add(t[0], t[1], t[2]);
+        let mut task_info = HashMap::new();
+        let mut heap = BinaryHeap::new();
+        
+        for task in tasks {
+            let user_id = task[0];
+            let task_id = task[1];
+            let priority = task[2];
+            
+            task_info.insert(task_id, (priority, user_id));
+            heap.push(Task { priority, task_id });
         }
-        tm
+        
+        Self { task_info, heap }
     }
-
+    
     fn add(&mut self, user_id: i32, task_id: i32, priority: i32) {
-        self.map.insert(task_id, (user_id, priority));
-        self.heap.push(Entry { priority, task_id });
+        self.task_info.insert(task_id, (priority, user_id));
+        self.heap.push(Task { priority, task_id });
     }
-
+    
     fn edit(&mut self, task_id: i32, new_priority: i32) {
-        if let Some((user, _)) = self.map.get_mut(&task_id) {
-            *self.map.get_mut(&task_id).unwrap() = (*user, new_priority);
-            self.heap.push(Entry { priority: new_priority, task_id });
+        if let Some(info) = self.task_info.get_mut(&task_id) {
+            info.0 = new_priority;
+            self.heap.push(Task { priority: new_priority, task_id });
         }
     }
-
+    
     fn rmv(&mut self, task_id: i32) {
-        self.map.remove(&task_id);
+        self.task_info.remove(&task_id);
     }
-
+    
     fn exec_top(&mut self) -> i32 {
-        while let Some(Entry { priority, task_id }) = self.heap.pop() {
-            if let Some(&(user, p)) = self.map.get(&task_id) {
-                if p == priority {
-                    self.map.remove(&task_id);
-                    return user;
+        while let Some(task) = self.heap.pop() {
+            if let Some(&(priority, user_id)) = self.task_info.get(&task.task_id) {
+                if priority == task.priority {
+                    self.task_info.remove(&task.task_id);
+                    return user_id;
                 }
             }
         }
