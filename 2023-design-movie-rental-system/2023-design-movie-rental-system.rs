64e040f59695e4a2c598e8d85c007424ee91d7b2
seokjin shin <sars21@hanmail.net>
2025-09-21 23:45:07 +0900

    Time: 63 ms (80%), Space: 66.2 MB (80%) - LeetHub

--- 2023-design-movie-rental-system/2023-design-movie-rental-system.rs
+++ 2023-design-movie-rental-system/2023-design-movie-rental-system.rs
@@ -1,105 +1,85 @@
-use std::{
-    cmp::Ordering,
-    collections::{BTreeSet, HashMap},
-};
+use std::collections::{HashMap, BTreeSet};
+use std::cmp::Ordering;
 
-#[derive(Eq, PartialEq, Clone)]
-struct Entry {
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct Pair {
+    first: i32,
+    second: i32,
+}
+
+impl Pair {
+    fn new(first: i32, second: i32) -> Self {
+        Pair { first, second }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Triple {
     price: i32,
     shop: i32,
     movie: i32,
 }
 
-impl Ord for Entry {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.price.cmp(&other.price) {
-            Ordering::Equal => match self.shop.cmp(&other.shop) {
-                Ordering::Equal => self.movie.cmp(&other.movie),
-                ord => ord,
-            },
-            ord => ord,
-        }
-    }
-}
-
-impl PartialOrd for Entry {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+impl Triple {
+    fn new(price: i32, shop: i32, movie: i32) -> Self {
+        Triple { price, shop, movie }
     }
 }
 
 struct MovieRentingSystem {
-    unrented: HashMap<i32, BTreeSet<Entry>>,
-    price: HashMap<(i32, i32), i32>,
-    rented: BTreeSet<Entry>,
+    t_price: HashMap<Pair, i32>,
+    t_valid: HashMap<i32, BTreeSet<Pair>>,
+    t_rent: BTreeSet<Triple>,
 }
 
 impl MovieRentingSystem {
-    fn new(_: i32, entries: Vec<Vec<i32>>) -> Self {
-        let mut unrented = HashMap::new();
-        let mut prices = HashMap::new();
-
+    fn new(n: i32, entries: Vec<Vec<i32>>) -> Self {
+        let mut t_price = HashMap::new();
+        let mut t_valid = HashMap::new();
+        
         for entry in entries {
             let shop = entry[0];
             let movie = entry[1];
             let price = entry[2];
-            let entry = Entry { price, shop, movie };
-
-            unrented
-                .entry(movie)
+            t_price.insert(Pair::new(shop, movie), price);
+            t_valid.entry(movie)
                 .or_insert_with(BTreeSet::new)
-                .insert(entry.clone());
-            prices.insert((shop, movie), price);
+                .insert(Pair::new(price, shop));
         }
-
-        Self {
-            unrented,
-            price: prices,
-            rented: BTreeSet::new(),
+        
+        MovieRentingSystem {
+            t_price,
+            t_valid,
+            t_rent: BTreeSet::new(),
         }
     }
-
+    
     fn search(&self, movie: i32) -> Vec<i32> {
-        self.unrented
-            .get(&movie)
-            .map(|set| set.iter().take(5).map(|e| e.shop).collect())
-            .unwrap_or_else(Vec::new)
+        self.t_valid.get(&movie)
+            .map_or_else(Vec::new, |set| {
+                set.iter()
+                    .take(5)
+                    .map(|p| p.second)
+                    .collect()
+            })
     }
-
+    
     fn rent(&mut self, shop: i32, movie: i32) {
-        if let Some(&p) = self.price.get(&(shop, movie)) {
-            let e = Entry {
-                price: p,
-                shop,
-                movie,
-            };
-            if let Some(set) = self.unrented.get_mut(&movie) {
-                set.remove(&e);
-            }
-            self.rented.insert(e);
-        }
+        let price = self.t_price[&Pair::new(shop, movie)];
+        self.t_valid.get_mut(&movie).unwrap().remove(&Pair::new(price, shop));
+        self.t_rent.insert(Triple::new(price, shop, movie));
     }
-
+    
     fn drop(&mut self, shop: i32, movie: i32) {
-        if let Some(&p) = self.price.get(&(shop, movie)) {
-            let e = Entry {
-                price: p,
-                shop,
-                movie,
-            };
-            self.rented.remove(&e);
-            self.unrented
-                .entry(movie)
-                .or_insert_with(BTreeSet::new)
-                .insert(e);
-        }
+        let price = self.t_price[&Pair::new(shop, movie)];
+        self.t_valid.get_mut(&movie).unwrap().insert(Pair::new(price, shop));
+        self.t_rent.remove(&Triple::new(price, shop, movie));
     }
-
+    
     fn report(&self) -> Vec<Vec<i32>> {
-        self.rented
-            .iter()
+        self.t_rent.iter()
             .take(5)
-            .map(|e| vec![e.shop, e.movie])
+            .map(|t| vec![t.shop, t.movie])
             .collect()
     }
 }
