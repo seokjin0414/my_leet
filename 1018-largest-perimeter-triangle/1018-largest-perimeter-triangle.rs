f6f033080cf6c4fe00315bb1b56e6a200ea3321d
seokjin shin <sars21@hanmail.net>
2025-09-28 23:34:34 +0900

    Time: 0 ms (100%), Space: 2.2 MB (72.73%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.rs
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.rs
@@ -1,12 +1,13 @@
 impl Solution {
-    pub fn largest_perimeter(mut nums: Vec<i32>) -> i32 {
+    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        
         nums.sort_unstable();
-        let mut result = 0;
-        for idx in 0..nums.len() - 2 {
-            if nums[idx] + nums[idx + 1] > nums[idx + 2] {
-                result = nums[idx] + nums[idx + 1] + nums[idx + 2];
-            }
-        }
-        result
+        nums.reverse();
+
+        nums.windows(3)
+            .find(|&win| win[0] < win[1] + win[2])
+            .map(|win| win.iter().sum())
+            .unwrap_or(0)
     }
 }
