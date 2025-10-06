24342f725e7c70994adf4ae368b2e2f58d2cb48e
seokjin shin <sars21@hanmail.net>
2025-10-06 22:36:40 +0900

    Time: 3 ms (100%), Space: 3.1 MB (72.22%) - LeetHub

--- 1100-connecting-cities-with-minimum-cost/1100-connecting-cities-with-minimum-cost.rs
+++ 1100-connecting-cities-with-minimum-cost/1100-connecting-cities-with-minimum-cost.rs
@@ -1,57 +1,64 @@
+use std::cmp::Ordering;
+
+struct UnionFind {
+    parents: Vec<usize>,
+    ranks: Vec<usize>,
+    groups: usize,
+    cost: i32,
+}
+
+impl UnionFind {
+
+    fn new(n: usize) -> Self {
+        let parents = (0..n).collect();
+        let ranks = vec![1; n];
+        let groups = n;
+        let cost = 0;
+        Self { parents, ranks, groups, cost }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parents[x] != x {
+            self.parents[x] = self.find(self.parents[x]);
+        }
+        return self.parents[x];
+    }
+
+    fn union(&mut self, x: usize, y: usize, cost: i32) -> () {
+        let px = self.find(x);
+        let py = self.find(y);
+        if px == py { return }
+        self.groups -= 1;
+        self.cost += cost;
+        match self.ranks[px].cmp(&self.ranks[py]) {
+            Ordering::Less => self.parents[px] = py,
+            Ordering::Greater => self.parents[py] = px,
+            Ordering::Equal => {
+                self.parents[px] = py;
+                self.ranks[py] += 1;
+            },
+        }
+    }
+}
+
+
 impl Solution {
     pub fn minimum_cost(n: i32, connections: Vec<Vec<i32>>) -> i32 {
-        let _len_cs: usize = connections.len();
-        let n: usize = n as usize;
-        let conn: Vec<Vec<i32>> = {
-            let mut conn = connections;
-            conn.sort_by_key(|e| e[2]);
-            conn
-        };
-        let mut roots: Vec<usize> = {
-            let mut roots = vec![0; n];
-            for idx in 0..n {
-                roots[idx] = idx;
-            }
-            roots
-        };
-        let mut ranks: Vec<u16> = vec![1; n];
-        let mut isolated: u16 = n as u16;
-        let mut ans: i32 = 0;
-        for c in conn {
-            let u: usize = c[0] as usize - 1;
-            let v: usize = c[1] as usize - 1;
-            let cost: i32 = c[2];
-            if Self::find(u, &mut roots) != Self::find(v, &mut roots) {
-                ans += cost;
-                Self::union(u, v, &mut roots, &mut ranks, &mut isolated);
-            }
+
+        let mut uf = UnionFind::new(n as usize);
+
+        let mut connections = connections;
+        connections.sort_unstable_by_key(|k| k[2]);
+        for con in connections {
+            let x = con[0] as usize - 1;
+            let y = con[1] as usize - 1;
+            let cost = con[2];
+            
+            uf.union(x, y, cost);
+
         }
-        if isolated == 1 {
-            ans
-        } else {
-            -1
-        }
-    }
-    fn union(u: usize, v: usize, roots: &mut Vec<usize>, ranks: &mut Vec<u16>, isolated: &mut u16) {
-        let root_u: usize = Self::find(u, roots);
-        let root_v: usize = Self::find(v, roots);
-        if root_u == root_v {
-            return;
-        }
-        if ranks[root_u] > ranks[root_v] {
-            roots[root_v] = root_u;
-            ranks[root_u] += 1;
-        } else {
-            roots[root_u] = root_v;
-            ranks[root_v] += 1;
-        }
-        *isolated -= 1;
-    }
-    fn find(mut x: usize, roots: &mut Vec<usize>) -> usize {
-        while x != roots[x] {
-            roots[x] = roots[roots[x]];
-            x = roots[x];
-        }
-        x
+
+        if uf.groups != 1 { return -1 }
+        return uf.cost;
     }
 }
