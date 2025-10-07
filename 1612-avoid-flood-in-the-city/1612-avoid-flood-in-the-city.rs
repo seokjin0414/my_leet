cd1cf27bca2f9ed238af1e9940aa4866c67d3228
seokjin shin <sars21@hanmail.net>
2025-10-07 20:50:33 +0900

    Time: 16 ms (85.71%), Space: 6.5 MB (14.29%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.rs
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.rs
@@ -1,27 +1,27 @@
-use std::collections::{BTreeSet, HashMap};
-
 impl Solution {
     pub fn avoid_flood(rains: Vec<i32>) -> Vec<i32> {
-        let mut lakes  = HashMap::new();
-        let mut days   = BTreeSet::new();
-        let mut result = Vec::with_capacity(rains.len());
+        let mut sunny_days = std::collections::BTreeSet::new();
+        let mut last_full_days = std::collections::HashMap::with_capacity(rains.len());
+        let mut result = vec![-1; rains.len()];
 
-        for (today, lake) in rains.into_iter().enumerate() {
-            if lake == 0 {
-                days.insert(today);
-                result.push(1);
-            } else {
-                if let Some(filled) = lakes.insert(lake, today) {
-                    if let Some(&day) = days.range(filled..).next() {
-                        result[day] = lake;
-                        days.remove(&day);
+        for (day, lake) in rains.into_iter().enumerate() {
+            if lake != 0 {
+                if let Some(last_day) = last_full_days.get(&lake) {
+                    if let Some(&sunny_day) = sunny_days.range(last_day + 1..).next() {
+                        sunny_days.remove(&sunny_day);
+                        result[sunny_day] = lake;
                     } else {
-                        return Vec::default();
+                        return Vec::new();
                     }
                 }
-                result.push(-1);
+
+                last_full_days.insert(lake, day);
+            } else {
+                sunny_days.insert(day);
+                result[day] = 1;
             }
         }
+
         result
     }
 }
