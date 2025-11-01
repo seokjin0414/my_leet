173ffe01e3e017c0d886124fd8342c3ed0e3ed29
seokjin <sars21@hanmail.net>
2025-11-01 23:18:32 +0900

    Time: 21 ms (20%), Space: 10.8 MB (20%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.rs
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.rs
@@ -1,13 +1,19 @@
 impl Solution {
-    pub fn modified_list(nums: Vec<i32>, head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        std::iter::successors(head.as_deref(), |node| node.next.as_deref())
-            .scan(
-                nums.into_iter().collect::<std::collections::HashSet<_>>(),
-                |f, n| Some((!f.contains(&n.val)).then_some(n.val)),
-            )
-            .flatten()
-            .collect::<Vec<_>>()
-            .into_iter()
-            .rfold(None, |next, val| Some(Box::new(ListNode { val, next })))
+    pub fn modified_list(nums: Vec<i32>, mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let set: std::collections::HashSet<_> = nums.into_iter().collect();
+
+        while matches!(head.as_ref(), Some(node) if set.contains(&node.val)) {
+            head = head.take().unwrap().next;
+        }
+
+        let mut current = head.as_mut();
+        while let Some(node) = current {
+            while matches!(node.next.as_ref(), Some(next) if set.contains(&next.val)) {
+                node.next = node.next.take().unwrap().next;
+            }
+            current = node.next.as_mut();
+        }
+
+        head
     }
 }
