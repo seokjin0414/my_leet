31344fd4102a3a42dcff36f3a18d74414cf575b8
seokjin <sars21@hanmail.net>
2025-11-09 16:22:32 +0900

    Time: 0 ms (100%), Space: 2.1 MB (35.29%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn count_operations(mut num1: i32, mut num2: i32) -> i32 {
+        let mut ops = 0;
+        while num1 > 0 && num2 > 0 {
+            if num1 > num2 {
+                std::mem::swap(&mut num1, &mut num2);
+            }
+            ops += num2 / num1;
+            num2 %= num1;
+        }
+        ops
+    }
+}
