2037aea36b7ec897928d85b8846561ff68e1df2e
seokjin <sars21@hanmail.net>
2025-11-28 23:27:13 +0900

    Time: 23 ms (40%), Space: 8.7 MB (80%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.rs
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.rs
@@ -1,32 +1,42 @@
-use std::collections::HashMap;
-
 impl Solution {
-    fn dfs(seen: &mut Vec<bool>, G: &HashMap<usize, Vec<usize>>, values: &mut Vec<i32>, k: &i32, u: usize, ans: &mut i32) -> i32 {
-        seen[u] = true;
-        if !G.contains_key(&u) { 
-            *ans += 1;
-            return 0;
-        }
-        for v in G[&u].iter() {
-            if !seen[*v] {
-                values[u] += Self::dfs(seen, G, values, k, *v, ans) % k;
+    pub fn max_k_divisible_components(n: i32, edges: Vec<Vec<i32>>, values: Vec<i32>, k: i32) -> i32 {
+    let mut adj: Vec<Vec<i32>> = vec![vec![]; n as usize];
+
+    for edge in edges.iter() {
+        adj[edge[0] as usize].push(edge[1]);
+        adj[edge[1] as usize].push(edge[0]);
+    }
+
+    let mut visited = vec![false; n as usize];
+    let mut ans = 0;
+
+    fn dfs(
+        root: i32,
+        values: &Vec<i32>,
+        k: i32,
+        adj: &Vec<Vec<i32>>,
+        visited: &mut Vec<bool>,
+        ans: &mut i32,
+    ) -> i32 {
+        let mut sum = values[root as usize];
+        visited[root as usize] = true;
+        for child in adj[root as usize].iter() {
+            if !visited[*child as usize] {
+                sum += dfs(*child, values, k, adj, visited, ans);
+                sum %= k;
             }
         }
-        if values[u] % k == 0 {
+
+        if sum % k == 0 {
             *ans += 1;
-            return 0;
+            0
+        } else {
+            sum
         }
-        values[u]
     }
-    pub fn max_k_divisible_components(n: i32, edges: Vec<Vec<i32>>, mut values: Vec<i32>, k: i32) -> i32 {
-        let mut G: HashMap<usize, Vec<usize>> = HashMap::with_capacity(n as usize);
-        for e in edges {
-            G.entry(e[0] as usize).or_insert(Vec::new()).push(e[1] as usize);
-            G.entry(e[1] as usize).or_insert(Vec::new()).push(e[0] as usize);
-        }
-        let mut seen: Vec<bool> = vec![false; n as usize];
-        let mut ans: i32 = 0;
-        Self::dfs(&mut seen, &G, &mut values, &k, 0, &mut ans);
-        ans
-    }
+
+    dfs(0, &values, k, &adj, &mut visited, &mut ans);
+
+    ans
+}
 }
