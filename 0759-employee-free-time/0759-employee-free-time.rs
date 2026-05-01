e9d5038b2d8fea628196001ab514d274cd9dab82
xin <sars21@hanmail.net>
2026-05-01 20:24:27 +0900

    Time: 5 ms (100%), Space: 2.6 MB (0%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.rs
+++ 0759-employee-free-time/0759-employee-free-time.rs
@@ -1,78 +1,39 @@
-use std::cmp::{Ordering, Reverse};
-use std::collections::BinaryHeap;
-
-#[repr(transparent)]
-#[derive(Debug, Eq, PartialEq)]
-struct EmployeeSchedule {
-    intervals: Vec<Interval>,
+/*
+// Definition for an Interval.
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct Interval {
+    pub start:i32,
+    pub end:i32
 }
 
-impl EmployeeSchedule {
+impl Interval {
     #[inline]
-    fn is_empty(&self) -> bool {
-        self.intervals.is_empty()
+    fn new(start:i32, end:i32) -> Self{
+        Interval {
+            start,
+            end
+        }
     }
 }
-
-impl From<Vec<Interval>> for EmployeeSchedule {
-    #[inline]
-    fn from(mut intervals: Vec<Interval>) -> Self {
-        intervals.reverse();
-        Self { intervals }
-    }
-}
-
-impl PartialOrd for Interval {
-    #[inline]
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.start.partial_cmp(&other.start)
-    }
-}
-
-impl PartialOrd<Self> for EmployeeSchedule {
-    #[inline]
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.intervals.last().partial_cmp(&other.intervals.last())
-    }
-}
-
-impl Ord for EmployeeSchedule {
-    #[inline]
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap_or(Ordering::Less)
-    }
-}
+*/
 
 impl Solution {
     pub fn employee_free_time(schedule: Vec<Vec<Interval>>) -> Vec<Interval> {
-        let mut heap = schedule
-            .into_iter()
-            .filter(|intervals| !intervals.is_empty())
-            .map(EmployeeSchedule::from)
-            .map(Reverse)
-            .collect::<BinaryHeap<_>>();
+        let mut data = vec![];
 
-        let mut end = if let Some(Reverse(mut schedule)) = heap.pop() {
-            let end = schedule.intervals.pop().unwrap().end;
-            heap.push(Reverse(schedule));
-            end
-        } else {
-            return vec![];
-        };
+        for s in schedule {
+            for p in s { data.push((p.start, p.end)); }
+        } 
+        data.sort();
 
-        let mut free = vec![];
-        while let Some(Reverse(mut schedule)) = heap.pop() {
-            if schedule.is_empty() {
-                continue;
-            }
-            let interval = schedule.intervals.pop().unwrap();
-            if interval.start > end {
-                free.push(Interval::new(end, interval.start));
-            }
-            end = end.max(interval.end);
-            heap.push(Reverse(schedule));
+        let mut last = data[0].1;     
+        let mut ret = vec![];
+        
+        for d in data {
+            if d.0 > last { ret.push(Interval::new(last, d.0)); }
+            last = last.max(d.1); 
         }
 
-        free
+        ret
     }
 }
